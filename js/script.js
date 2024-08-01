923c3b545784d247e7163ad3f4d85c3b23d38991
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-08-01 20:52:23 +0200

    ho modificato le informazioni nell array perche mi sono appena accorto di avere quelle specifiche in allegato nella consegna

--- js/script.js
+++ js/script.js
@@ -15,32 +15,29 @@ Aggiungere il ciclo infinito del carosello. Ovvero se la slide attiva è la prim
 
 */
 const array = [
-   {
-    image: '01.webp',
-    tittle: 'image1',
-    descrizione : 'image1'
-   },
-   {
-    image: '02.webp',
-    tittle: 'image2',
-    descrizione : 'image2'
-   },
-   {
-    image: '03.webp',
-    tittle: 'image3',
-    descrizione : 'image3 '
-   },
-   {
-    image: '04.webp',
-    tittle: 'image4',
-    descrizione : 'image4 '
-   },
-   {
-    image: '05.webp',
-    tittle: 'image5',
-    descrizione : 'image5 '
-   },
-];
+        {
+            image: 'img/01.webp',
+            title: 'Marvel\'s Spiderman Miles Morale',
+            text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        }, {
+            image: 'img/02.webp',
+            title: 'Ratchet & Clank: Rift Apart',
+            text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        }, {
+            image: 'img/03.webp',
+            title: 'Fortnite',
+            text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        }, {
+            image: 'img/04.webp',
+            title: 'Stray',
+            text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        }, {
+            image: 'img/05.webp',
+            title: "Marvel's Avengers",
+            text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        }
+    ];
+
 for ( let i = 0; i < array.length; i++){
     console.log(array[i]);
 };
